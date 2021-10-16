af13323c299c67e1230dddb50790438e95c247db
pkhatri196 <84386920+pkhatri196@users.noreply.github.com>
2021-10-16 16:25:41 +0530

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -1,23 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
-import "./index.css";
-import App from "./App.jsx";
-import { Provider } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 
-import Store from "./Redux/store";
+// components
+import DeliveryCarousal from "./DeliveryCarousal";
+import Brand from "./Brand";
+import RestaurantCard from "../RestaurantCard";
 
-// Import css files
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
+const Delivery = () => {
+  const [restaurantList, setRestaurantList] = useState([]);
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={Store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+  const reduxState = useSelector(
+    (globalStore) => globalStore.restaurant.restaurants
+  );
+
+  useEffect(() => {
+    reduxState.restaurants && setRestaurantList(reduxState.restaurants);
+  }, [reduxState.restaurants]);
+
+  return (
+    <>
+      <DeliveryCarousal />
+      {/* <Brand /> */}
+      <h1 className="text-xl mt-4 mb-2 md:mt-8 md:text-3xl md:font-semibold">
+        Delivery Restaurants in Vijay Nagar
+      </h1>
+      <div className="flex justify-between flex-wrap">
+        {restaurantList.map((restaurant) => (
+          <RestaurantCard
+            {...restaurant}
+            key={restaurant._id}
+            whereIsthisres="asf"
+          />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Delivery;
